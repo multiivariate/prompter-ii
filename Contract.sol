93f35fe3c93ae4f40c2e4b4991684568594f3e67
multiivariate <53394519+multiivariate@users.noreply.github.com>
2023-02-05 16:50:03 +0300

    Update Contract.sol

--- Contract.sol
+++ Contract.sol
@@ -10,6 +10,7 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract Prompter is ERC721Enumerable, Ownable {
     using Strings for uint256;
@@ -32,69 +33,76 @@ contract Prompter is ERC721Enumerable, Ownable {
     uint256 public privateSupply = 1260;
     uint256 public wlSupply = 1000;
 
-    Status public saleStatus = Status.Inactive;
+    bytes32 private _merkleRootWL;
+    bytes32 private _merkleRootPrivate;
+
+    Status public saleStatus;
     
-    constructor() ERC721("Prompter", "TPC") {}
+    constructor() ERC721("Prompter II", "TPC") {
+        saleStatus = Status.Inactive;
+    }
 
     function mint(string memory _prompt) public payable {
-        require(getStatus() == 3, "Public sale isn't active.");
-        require(bytes(_prompt).length < 422, "Max length 421 // Only base64 characters");
-        require(prompts[_prompt] == false, "This prompt claimed.");
-        require(msg.value >= price, "Art isn't expensive.");
+        require(uint256(saleStatus) == 3, "Public sale isn't active.");
         require(promptCount[msg.sender] < maxPerWallet + 1, "You can mint up to 5 tokens.");
         require(totalSupply() < maxSupply + 1, "Sold out.");
+        checkRequirements(price, _prompt);
 
-        prompts[_prompt] = true;
-        promptsWithIDs[totalSupply() + 1] = _prompt;
-        promptCount[msg.sender] += 1;
-
-        _safeMint(msg.sender, totalSupply() + 1);
+        claimPrompt(_prompt);
     }
 
-    function mintWL(string memory _prompt) public payable {
-        // check wallet for whitelist
-        require(getStatus() == 2, "Whitelist sale isn't active.");
-        require(bytes(_prompt).length < 422, "Max length 421 // Only base64 characters");
-        require(prompts[_prompt] == false, "This prompt claimed.");
-        require(msg.value >= wlPrice, "Art isn't expensive.");
+    function mintWL(string memory _prompt, bytes32[] calldata _merkleProof) public payable {
+        require(uint256(saleStatus) == 2, "Whitelist sale isn't active.");
         require(whitelistCount[msg.sender] < maxPerWalletWL + 1, "You can mint up to 2 tokens.");
         require(totalSupply() < wlSupply + 1, "WL ended.");
+        require(MerkleProof.verify(_merkleProof, _merkleRootWL, keccak256(abi.encodePacked(msg.sender))), "You're not whitelisted.");
+        checkRequirements(wlPrice, _prompt);
 
+        whitelistCount[msg.sender] += 1;
+        claimPrompt(_prompt);
+    }
+
+    function mintPrivate(string memory _prompt, bytes32[] calldata _merkleProof) public {
+        require(uint256(saleStatus) == 1, "Private sale isn't active.");
+        require(privateCount[msg.sender] < maxPerWalletPrivate + 1, "You can mint up to 3 tokens.");
+        require(totalSupply() < privateSupply + 1, "Private sale ended.");
+        require(MerkleProof.verify(_merkleProof, _merkleRootPrivate, keccak256(abi.encodePacked(msg.sender))), "You don't have free mint pass.");
+        checkRequirements(0, _prompt);
+
+        privateCount[msg.sender] += 1;
+        claimPrompt(_prompt);
+    }
+
+    function checkRequirements(uint256 minPrice, string memory _prompt) internal {
+        require(bytes(_prompt).length < 422, "Max length 421 // Only base64 characters");
+        require(prompts[_prompt] == false, "This prompt claimed.");
+        require(msg.value >= minPrice, "Art isn't expensive.");
+    }
+
+    function claimPrompt(string memory _prompt) internal {
         prompts[_prompt] = true;
         promptsWithIDs[totalSupply() + 1] = _prompt;
         promptCount[msg.sender] += 1;
-        whitelistCount[msg.sender] += 1;
 
         _safeMint(msg.sender, totalSupply() + 1);
     }
 
-    function mintPrivate(string memory _prompt) public {
-        // check wallet for private
-        require(getStatus() == 1, "Private sale isn't active.");
-        require(bytes(_prompt).length < 422, "Max length 421 // Only base64 characters");
-        require(prompts[_prompt] == false, "This prompt claimed.");
-        require(privateCount[msg.sender] < maxPerWalletPrivate + 1, "You can mint up to 3 tokens.");
-        require(totalSupply() < privateSupply + 1, "Private sale ended.");
-
-        prompts[_prompt] = true;
-        promptsWithIDs[totalSupply() + 1] = _prompt;
-        promptCount[msg.sender] += 1;
-        privateCount[msg.sender] += 1;
-
-        _safeMint(msg.sender, totalSupply() + 1);
+    function adminMint(string memory _prompt) public onlyOwner {
+        claimPrompt(_prompt);
     }
 
     function buildImage(string memory _Prompt) internal pure returns (string memory) {
         return
             Base64.encode(
                 abi.encodePacked(
-                    '<svg width="500" height="500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">',
-                    '<rect height="100%" width="100%" y="0" x="0" fill="#0c0c0c"/>',
+                    '<svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">',
+                    '<rect height="100%" width="100%" y="0" x="0" fill="#f5f5f5"/>',
                     '<defs>',
-                    '<path id="path1" d="M7.55,33.94H484M7.55,67.38H484M7.3,100.83H483.74M7.44,134.24H483.88M7.44,167.67H483.88M7.44,201.11H483.88M7.18,234.54H483.62M7.74,267.97H484.19M7.74,301.41H484.19M7.49,334.86H483.92M7.63,368.27H484.07M7.63,401.7H484.07M7.63,435.14H484.07M7.37,468.57H483.8"></path>',
+                    '<path id="path1" d="M29.43,81.73H970.93M29.43,147.1H970.93M28.93,212.5H970.41M29.21,277.82H970.69M29.21,343.17H970.69M29.21,408.55H970.69M28.7,473.91H970.18M29.8,539.26H971.3M29.8,604.64H971.3M29.31,670.04H970.77M29.59,735.35H971.07M29.59,800.71H971.07M29.59,866.09H971.07M29.07,931.44H970.53"></path>',
+                    '<style type="text/css">@import url(http://fonts.googleapis.com/css?family=Space+Grotesk);</style>',
                     '</defs>',
                     '<use xlink:href="#path1" />',
-                    '<text font-size="26.47px" fill="whitesmoke" font-family="Courier New">',
+                    '<text font-size="52.2px" fill="#0c0c0c" font-weight="500" font-family="Space Grotesk">',
                     '<textPath xlink:href="#path1">', _Prompt,'</textPath></text>',
                     '</svg>'
                 )
@@ -105,22 +113,22 @@ contract Prompter is ERC721Enumerable, Ownable {
     {
         require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
 
-        return string(
-                    string.concat(
-                        "data:application/json;base64,",
+        return
+            string(
+                string.concat(
+                    "data:application/json;base64,",
+                    bytes(
                         Base64.encode(
-                            abi.encodePacked('{"name":"', abi.encodePacked("#", _tokenId.toString()),'",', '"description":"Prompter is a collection by You and Monas."', '"image":"data:image/svg+xml;base64', buildImage(promptsWithIDs[_tokenId]), '"}')
+                            abi.encodePacked(
+                                '{"name":"', abi.encodePacked("#", _tokenId.toString()),'",',
+                                '"description":"Prompter is a collection by You and Monas.",',
+                                '"image":"data:image/svg+xml;base64', buildImage(promptsWithIDs[_tokenId]), '",',
+                                '"attributes": [{"Length": "', bytes(promptsWithIDs[_tokenId]).length ,'"}]}'
+                            )
                         )
                     )
-                );
-    }
-
-    function getPrice() public view returns (uint256) {
-        return price;
-    }
-    
-    function getStatus() public view returns (uint256) {
-        return uint256(saleStatus);
+                )
+            );
     }
 
     function setSaleStatus(Status _status) public onlyOwner {
